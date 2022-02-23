260344836e3553604483b44b6af431c321d47184
Harisha V S <harisha86@yahoo.co.uk>
2022-02-23 13:02:04 +0530

    Updated Controller
    New methods introduced.

--- src/main/java/com/playground/codehub/controller/CodeHubRestController.java
+++ src/main/java/com/playground/codehub/controller/CodeHubRestController.java
@@ -1,6 +1,10 @@
 package com.playground.codehub.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +12,19 @@ public class CodeHubRestController {
     @GetMapping("/")
 	public String Welcome() {
 		return "Welcome to home";
+	}
+
+	@GetMapping("/list")
+	public List getList() {
+		List getAll = new ArrayList<>();
+		getAll.add("one");
+		getAll.add("two");
+		getAll.add("one");
+		return getAll;
+	}
+
+	@GetMapping("/{name}")
+	public String dynamicWelcome(@PathVariable String name) {
+		return name+", "+"Welcome to home";
 	}
 }
